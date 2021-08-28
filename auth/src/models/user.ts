e844802cac8213fbfc36d4c62652e92612291e3b
vineet-codes <vineetwrites@gmail.com>
2021-08-28 12:17:16 +0100

    User MongoDB model and integration with typescript

---
+++ auth/src/models/user.ts
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+import { Password } from '../services/password';
+
+// this is an interface to describe the properties
+// that are required to create an User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User Document has
+// UserDoc defines structure for what we get back from mongo when we interact with it
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User model has
+// define what additional methods we add to mongo Model
+interface UserModel extends mongoose.Model<UserDoc> {
+  // use Build to create a new user which accepts
+  // objects of UserAttrs type and return a user of UserDoc type
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// pre-save hook for monpgoDB,
+// it automatically hash the password when attempting to save the details
+// we get access to the document we save as this inside the function
+// hence we need to define normal function and not arrow function
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+
+    this.set('password', hashed);
+  }
+  done();
+});
+
+// this is how we add functions/functionality to a mongoose model
+// we have to define {UserAttrs and UserModel} interfaces, so that
+// typescript helps us when we create new Users through the build function
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+// we extend the model type with UserModel interface this way (<> using angle brackets)
+// : need to learn some theory to internalize it
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+// create a new user like below and typescript will help you everywhere in this application
+// const user = User.build({
+//   email: 'test@test.com',
+//   password: 'password',
+// });
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { User } from './../models/user';
+
 // custom sub-classes for error handling
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request';
 
 const router = express.Router();
 
@@ -16,20 +18,32 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     //
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
+    // User Signup flow
+    // 1. Does a user with this email already exists ? If so, respond with error : CutomError type of BadRequestError
+    // 2. Can't store passwords in plain text, Hash the password user entered : this is done via Pre save hook of mongoose
+    // 3. Create a new User and save them to MongoDB
+    // 4. User is now considered logged in, Send them a cookie/jwt etc
+
     const { email, password } = req.body;
 
-    console.log('Creating a user ......');
-    // simulating data base error
-    throw new DatabaseConnectionError();
+    // find and check if a user with email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-    res.send({});
+    // create a new user and save to db
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
