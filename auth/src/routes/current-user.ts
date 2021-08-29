15517426241f711c5a36b5e70f99f8ac7a4d74cf
vineet-codes <vineetwrites@gmail.com>
2021-08-29 12:30:33 +0100

    current-user, require auth middle wares, not authorized error

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,15 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   // 1. Does this user have a `req.session.jwt` set ?
   // 2. If it is not set, or if the JWT is invalid, return early
   // 3. If yes, and JWT is valid, send back the info stored inside the JWT (the payload)
 
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
