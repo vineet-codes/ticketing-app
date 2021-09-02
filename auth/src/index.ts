96c045aba53040202ed7f480d1297ae7ddbb4f0d
vineet-codes <vineetwrites@gmail.com>
2021-09-02 23:59:26 +0100

    k8 setup and skaffold for tickets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -11,8 +11,12 @@ const start = async () => {
     throw new Error('Environment variable JWT_KEY not defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('Environment variable JWT_KEY not defined');
+  }
+
   try {
-    await mongoose.connect(`mongodb://auth-mongo-srv:27017/auth`, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
